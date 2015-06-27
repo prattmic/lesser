91baf8de9889ed5053fcff1202c09233a0a0ebd2
Michael Pratt <michael@pratt.im>
2015-06-26 17:53:56 -0700

    Create a channel of Lesser events
    The main goroutine handles all events.

--- lesser.go
+++ lesser.go
@@ -6,23 +6,46 @@ import (
 	"github.com/nsf/termbox-go"
 )
 
+type Event int
+
+const (
+	EventQuit Event = iota
+)
+
 type Lesser struct {
 	file *os.File
+
+	events chan Event
 }
 
-func (l Lesser) Run() {
+func (l Lesser) listenEvents() {
 	for {
 		e := termbox.PollEvent()
 		switch e.Type {
 		case termbox.EventKey:
 			switch e.Ch {
 			case 'q':
-				return
+				l.events <- EventQuit
 			}
 		}
 	}
 }
 
+func (l Lesser) Run() {
+	go l.listenEvents()
+
+	select {
+	case e := <-l.events:
+		switch e {
+		case EventQuit:
+			return
+		}
+	}
+}
+
 func NewLesser(f *os.File) Lesser {
-	return Lesser{file: f}
+	return Lesser{
+		file:   f,
+		events: make(chan Event, 1),
+	}
 }
